3c00fb9f5a177bb50c62830550fc8b5de061d003
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-08-27 16:46:11 -0400

    installed pgcli as regular dependency

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DB_URL || "postgresql://ryannicoletti@localhost/trello",
+    process.env.DATABASE_URL ||
+    "postgresql://ryannicoletti@localhost/trello",
 };
