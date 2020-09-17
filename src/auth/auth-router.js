7287124cf9249001f924116bddc6990361f56142
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-09-17 00:04:33 -0400

    configured cors for login post request

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,41 +1,51 @@
 const express = require("express");
 const authService = require("./auth-service");
 const { requiresAuthorization } = require("../middleware/jwt-auth");
-
+const cors = require("cors");
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
-authRouter.post("/login", jsonBodyParser, (req, res, next) => {
-  const { email, password } = req.body;
-  const userLoggingIn = { email, password };
+const corsOptions = {
+  origin: "http://localhost:3000/login",
+  optionsSuccessStatus: 200,
+};
 
-  for (const [key, value] of Object.entries(userLoggingIn))
-    if (value == null)
-      return res
-        .status(400)
-        .json({ error: `Missing '${key}' in request body` });
-  authService
-    .confirmUserNameExists(req.app.get("db"), userLoggingIn.email)
-    .then((userInDb) => {
-      if (!userInDb)
+authRouter.post(
+  "/login",
+  cors(corsOptions),
+  jsonBodyParser,
+  (req, res, next) => {
+    const { email, password } = req.body;
+    const userLoggingIn = { email, password };
+
+    for (const [key, value] of Object.entries(userLoggingIn))
+      if (value == null)
         return res
           .status(400)
-          .json({ error: "Incorrect email or password" });
-    });
-  return authService
-    .comparePasswords(userLoggingIn.password, userInDb.password)
-    .then((matchedPw) => {
-      if (!matchedPw)
-        return res
-          .status(400)
-          .json({ error: "Incorrect email or password" });
+          .json({ error: `Missing '${key}' in request body` });
+    authService
+      .confirmUserNameExists(req.app.get("db"), userLoggingIn.email)
+      .then((userInDb) => {
+        if (!userInDb)
+          return res
+            .status(400)
+            .json({ error: "Incorrect email or password" });
+      });
+    return authService
+      .comparePasswords(userLoggingIn.password, userInDb.password)
+      .then((matchedPw) => {
+        if (!matchedPw)
+          return res
+            .status(400)
+            .json({ error: "Incorrect email or password" });
 
-      const subject = userInDb.email;
-      const payload = { userId: userInDb.id };
-      res.send({ authToken: authService.createJwt(subject, payload) });
-    })
-    .catch(next);
-});
+        const subject = userInDb.email;
+        const payload = { userId: userInDb.id };
+        res.send({ authToken: authService.createJwt(subject, payload) });
+      })
+      .catch(next);
+  }
+);
 
 authRouter.post("/refresh", requiresAuthorization, (req, res) => {
   const subject = req.user.email;
