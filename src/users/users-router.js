2f5c797fe93abf24bbf08e57d2c39be68c6063e0
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-09-30 06:05:06 -0400

    added auth token creation to new user sign up

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,46 +1,67 @@
 const express = require("express");
 const path = require("path");
 const UsersService = require("./users-service");
-
+const authService = require("../auth/auth-service");
+const cors = require("cors");
 const usersRouter = express.Router();
 const jsonBodyParser = express.json();
+const config = require("../config");
 
-usersRouter.post("/", jsonBodyParser, (req, res, next) => {
-  const { email, password } = req.body;
-  for (const field of ["email", "password"])
-    if (!req.body[field])
-      return res
-        .status(400)
-        .json({ error: `Missing '${field}' in request body` });
+const corsOptions = {
+  origin: `${config.REQUEST_ORIGIN}`,
+  optionsSuccessStatus: 200,
+};
 
-  const passwordError = UsersService.validatePassword(password);
-  if (passwordError) return res.status(400).json({ error: passwordError });
+usersRouter.post(
+  "/",
+  cors(corsOptions),
+  jsonBodyParser,
+  (req, res, next) => {
+    const { email, password } = req.body;
+    for (const field of ["email", "password"])
+      if (!req.body[field])
+        return res
+          .status(400)
+          .json({ error: `Missing '${field}' in request body` });
 
-  UsersService.containsUserWithEmailAddress(req.app.get("db"), email)
-    .then((hasEmailAddress) => {
-      if (hasEmailAddress)
-        return res.status(400).json({ error: "Email already taken" });
+    const passwordError = UsersService.validatePassword(password);
+    if (passwordError)
+      return res.status(400).json({ error: passwordError });
 
-      return UsersService.hashPassword(password).then((hashedPassword) => {
-        const newUser = {
-          email,
-          password: hashedPassword,
-          date_created: "now()",
-        };
+    UsersService.containsUserWithEmailAddress(req.app.get("db"), email)
+      .then((hasEmailAddress) => {
+        if (hasEmailAddress)
+          return res.status(400).json({ error: "Email already taken" });
 
-        return UsersService.insertUser(req.app.get("db"), newUser).then(
-          (user) => {
-            res
-              .status(201)
+        return UsersService.hashPassword(password).then(
+          (hashedPassword) => {
+            const newUser = {
+              email,
+              password: hashedPassword,
+              date_created: "now()",
+            };
 
-              .location(path.posix.join(req.originalUrl, `/${user.id}`))
-              .json(UsersService.serializeUser(user));
+            return UsersService.insertUser(
+              req.app.get("db"),
+              newUser
+            ).then((user) => {
+              const subject = user.email;
+              const payload = { userId: user.id };
+              res.send({
+                authToken: authService.createJwt(subject, payload),
+              });
+              return res
+                .status(201)
+
+                .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                .json(UsersService.serializeUser(user));
+            });
           }
         );
-      });
-    })
+      })
 
-    .catch((error) => console.log(error));
-});
+      .catch((error) => console.log(error));
+  }
+);
 
 module.exports = usersRouter;
