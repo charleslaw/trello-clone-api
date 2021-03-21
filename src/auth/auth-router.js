319dc7e8b4c9edd0e3c14770dcb9df2f26b8c89e
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2021-03-21 10:02:20 -0400

    trouble shoot login bug

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const authService = require("./auth-service");
 const { requiresAuthorization } = require("../middleware/jwt-auth");
-// const cors = require("cors");
+const cors = require("cors");
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 const config = require("../config");
 
-// const corsOptions = {
-//   origin: `${config.REQUEST_ORIGIN}`,
-//   optionsSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: `${config.REQUEST_ORIGIN}`,
+  optionsSuccessStatus: 200,
+};
 
 // handle post request to login end point (user logging in)
 authRouter.post(
   "/login",
-  // cors(corsOptions),
+  cors(corsOptions),
   jsonBodyParser,
   async (req, res, next) => {
     const { email, password } = req.body;
